ee9299765cfe859756d8826d1420977291f07368
PeteHack <pwachira900@gmail.com>
2021-09-02 11:38:07 +0300

    Add theme, permissions , colors and extension functions

---
+++ app/src/main/java/com/droid/newsapiclient/data/util/extensions/ViewExt.kt
@@ -0,0 +1,128 @@
+package com.droid.newsapiclient.data.util.extensions
+
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.view.View
+import android.view.View.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.droid.newsapiclient.R
+import com.google.android.material.snackbar.Snackbar
+
+
+fun View.visible(isVisible: Boolean) {
+    visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+fun View.show() {
+    visibility = VISIBLE
+}
+
+fun View.hide() {
+    visibility = GONE
+}
+
+fun View.makeInvisible() {
+    visibility = INVISIBLE
+}
+
+fun View.enable() {
+    isEnabled = true
+}
+
+fun View.disable() {
+    isEnabled = false
+}
+
+fun Context.toast(message: String, length: Int) {
+    Toast.makeText(this, message, length).show()
+}
+
+
+fun View.showSnackbar(message: String, length: Int) {
+    val snackbar = Snackbar.make(this, message, length)
+    snackbar.apply {
+        setTextColor(ContextCompat.getColor(this.context, android.R.color.white))
+        this.setBackgroundTint(ContextCompat.getColor(context, R.color.colorPrimary))
+        show()
+
+    }
+}
+
+internal fun View.setBackgroundTint(context: Context, color: Int) {
+    backgroundTintList = ColorStateList.valueOf(
+        ContextCompat.getColor(context, color)
+    )
+}
+
+
+fun View.showErrorSnackbar(message: String, length: Int) {
+    val snackbar = Snackbar.make(this, message, length)
+
+    snackbar.apply {
+        this.setBackgroundTint(ContextCompat.getColor(this.context, android.R.color.holo_red_light))
+        this.setTextColor(ContextCompat.getColor(this.context, android.R.color.white))
+        show()
+    }
+}
+
+fun View.showSuccessSnackbar(message: String, length: Int) {
+    val snackbar = Snackbar.make(this, message, length)
+
+    snackbar.apply {
+        this.setBackgroundTint(
+            ContextCompat.getColor(view.context, R.color.colorPrimary)
+        )
+        this.setTextColor(ContextCompat.getColor(this.context, android.R.color.white))
+        show()
+    }
+}
+
+fun Activity.hideSoftInput() {
+    val inputmethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputmethodManager.hideSoftInputFromWindow(getView().windowToken, 0)
+}
+
+fun Activity.getView(): View {
+    return window.decorView.rootView
+}
+
+fun View.showRetrySnackBar(message: String, action: ((View) -> Unit)?) {
+    val snackbar = Snackbar.make(this, message, Snackbar.LENGTH_INDEFINITE)
+
+    snackbar.apply {
+        this.setBackgroundTint(ContextCompat.getColor(this.context, android.R.color.holo_red_light))
+
+        val colorWhite = ContextCompat.getColor(this.context, android.R.color.white)
+        this.setTextColor(colorWhite)
+        this.setActionTextColor(colorWhite)
+        setAction("RETRY") {
+            dismiss()
+            action?.invoke(this@showRetrySnackBar)
+        }
+        show()
+
+    }
+}
+
+internal inline fun <reified T> Activity.navigateTo(
+    clearTask: Boolean = false,
+    noinline intentExtras: ((Intent) -> Unit)? = null
+) {
+
+    val intent = Intent(this, T::class.java)
+
+    intentExtras?.run {
+        intentExtras(intent)
+    }
+
+    if (clearTask) {
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    startActivity(intent)
+}
