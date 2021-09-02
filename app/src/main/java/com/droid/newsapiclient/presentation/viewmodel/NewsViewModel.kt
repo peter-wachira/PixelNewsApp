b1897b3dcadc621a34a89ba76714d40635d2d4fe
PeteHack <pwachira900@gmail.com>
2021-09-02 11:39:24 +0300

    Create viewmodel and network check

---
+++ app/src/main/java/com/droid/newsapiclient/presentation/viewmodel/NewsViewModel.kt
@@ -0,0 +1,71 @@
+package com.droid.newsapiclient.presentation.viewmodel
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.droid.newsapiclient.data.model.APIResponse
+import com.droid.newsapiclient.data.util.Resource
+import com.droid.newsapiclient.domain.usecase.GetNewsHeadlinesUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class NewsViewModel(
+        private val app: Application,
+        private val getNewsHeadlinesUseCase: GetNewsHeadlinesUseCase
+) : AndroidViewModel(app) {
+    private val newsHeadlines: MutableLiveData<Resource<APIResponse>> = MutableLiveData()
+
+
+    fun getNewsHeadlines(country: String, page: Int) = viewModelScope.launch(Dispatchers.IO) {
+        newsHeadlines.postValue(Resource.Loading())
+
+        try {
+
+
+            if (isNetworkAvailable(app)) {
+                val apiResult = getNewsHeadlinesUseCase.execute(country, page)
+                newsHeadlines.postValue(apiResult)
+            } else {
+                newsHeadlines.postValue(Resource.Error("Internet not Available"))
+            }
+        }catch (e: Exception){
+            newsHeadlines.postValue(Resource.Error(e.message.toString()))
+        }
+    }
+
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
+
+    }
+}
