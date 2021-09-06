45f72f75fab1c9c0f92d11014c11a6dfc4b9b447
PeteHack <pwachira900@gmail.com>
2021-09-06 13:02:22 +0300

    create navigation items

--- app/src/main/java/com/droid/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/droid/newsapiclient/MainActivity.kt
@@ -2,10 +2,19 @@ package com.droid.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.droid.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+           ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+
+
 }
